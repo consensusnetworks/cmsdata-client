a853018b7de230ff75904793659137b544311de2
hawyar <hawyarxoshnaw@gmail.com>
2021-07-25 19:50:08 -0400

    Make ts client more stable, update tests, add fixtures

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,5 @@
 import fetch from "node-fetch";
-
+import * as Papa from "papaparse";
 interface QueryParams {
 	filter?: string
 	limit?: number;
@@ -7,7 +7,6 @@ interface QueryParams {
 	order?: string;
 	sortBy?: string;
 }
-
 
 interface Dataset {
   identifier: string | string[];
@@ -26,14 +25,25 @@ type Result = {
 	metadata?: object;
 	error: Error | null;
 }
+interface Options {
+	output: 'csv' | 'json',
+	stream: NodeJS.WritableStream | null
+}
+
+interface transformer {
+	data: object[];
+	// extract from first object
+	fields?: string[];	
+}
 
 class Client {
 	query: QueryParams;
 	dataset: Dataset;
-	options: fetchOptions;
+	fetchOptions: fetchOptions;
 	result: Result;
+	options: Options;
 	
-	constructor(datasetId: Dataset['identifier']) {
+	constructor(datasetId: Dataset['identifier'], options: Options) {
 		this.result = {
 			data: "",
 			error: null
@@ -48,7 +58,7 @@ class Client {
 		this.dataset = {
 			identifier: datasetId
 		}
-		this.options = {
+		this.fetchOptions = {
 			url: `https://data.cms.gov/data-api/v1/dataset/${datasetId}/data`,
 			method: 'GET',
 			headers: {
@@ -56,24 +66,29 @@ class Client {
 			},
 			params: this.query
 		}
-	}
-
-	async get() {
-		const p = await Promise.all([this.getResource(), this.getResourceMetadata()])
-		console.log(p)
-		return this.result;
+		this.options = {
+			output: options ? options.output :'json',
+			stream: null,
+		}
 	}
 
 	private async getResource() {
 		try {
-			const res = await fetch(this.options.url, { method: this.options.method })
+			const res = await fetch(this.fetchOptions.url, { method: this.fetchOptions.method })
 
 			if (res.status !== 200) {
 				this.result.error = new Error(`HTTP ${res.status}`);
 				return; 
 			}
-			const resource = await res.json()
-			this.result.data = resource
+
+			if (this.options.output === 'csv') {
+				let transform = {
+					data: await res.json(),
+				} 
+				this.result.data = await this.JSONToCSV(transform)
+			} else {
+				this.result.data = await res.json()
+			}
 			this.result.error = null
 		} catch (e: any) {
 			this.result.error = e;
@@ -81,9 +96,9 @@ class Client {
 	}
 
 	private async getResourceMetadata() {
-		const metadataEndpoint = `https://data.cms.gov/data-api/v1/dataset/${this.dataset.identifier}/data-viewer/`
+		const viewerEndpoint = `https://data.cms.gov/data-api/v1/dataset/${this.dataset.identifier}/data-viewer/`
 		try {
-			const res = await fetch(metadataEndpoint, { method: this.options.method })
+			const res = await fetch(viewerEndpoint, { method: this.fetchOptions.method })
 			if (res.status !== 200) {
 				this.result.error = new Error(`HTTP ${res.status}`);
 				return; 
@@ -95,10 +110,26 @@ class Client {
 			this.result.error = e;
 		}
 	}
+
+	private JSONToCSV(t: transformer): string {
+		const p =  Papa.unparse(t.data, {
+			header: true,
+			skipEmptyLines: false,
+			newline: '\n',
+			delimiter: ',',
+		})
+		return p;
+	} 
+
+	async get() {
+		await this.getResource()
+		await this.getResourceMetadata()
+		return this.result;
+	}
 }
 
-function createClient(datasetId: Dataset['identifier']): Client {
-	return new Client(datasetId);
+function createClient(datasetId: Dataset['identifier'], options: Options): Client {
+	return new Client(datasetId, options);
 }
 
 module.exports = {
--- tests/client.js
+++ tests/client.js
@@ -1,177 +1,26 @@
-// const os = require('os');
-// const path = require('path');
-// const fs = require('fs');
+const os = require('os');
+const path = require('path');
+const fs = require('fs');
+
 const tap = require('tap');
 const { createClient } = require('../build/client');
-// const { createClient } = require('../index');
-// const jsonDataset = require('./fixtures/5fr6-cch3.json');
 
-// tap.beforeEach((t) => {
-//   t.datasetId = '5fr6-cch3';
-// });
+tap.test('Test CMS Client', async (t) => {
+  const datasetId = 'b8b0419c-3025-4738-8787-ed40c9e0816d';
+  const client = createClient(datasetId);
+  const result = await client.get();
 
-tap.test('Test ts lib', async (t) => {
-  const client = createClient('b8b0419c-3025-4738-8787-ed40c9e0816d');
-  const { data, err } = await client.get();
-
-  if (err) {
-    console.error(err);
+  if (result.err) {
+    throw new Error(result.err);
   }
+  const file = fs.readFileSync(
+    path.join(__dirname, 'fixtures', `${datasetId}.json`),
+    'utf8',
+  );
 
-  console.log('fetched ' + data.length + ' records');
-  t.pass('just pass for now');
+  // only first 10
+  const wanted = JSON.parse(file).slice(0, 10);
+  const found = result.data.slice(0, 10);
+
+  t.same(found, wanted);
 });
-
-//   t.test('Without metadata', async (t) => {
-//     const CMSClient = createClient(t.datasetId, {
-//       output: 'json',
-//     });
-//     const dataset = await CMSClient.select().get();
-//     if (typeof dataset.metadata === 'undefined') {
-//       t.pass('metadata omitted');
-//     }
-//     t.same(dataset.data[0], jsonDataset[0], 'returns all records');
-//   });
-
-//   t.test('Without options', async (t) => {
-//     const CMSClient = createClient(t.datasetId);
-
-//     const dataset = await CMSClient.get();
-//     t.same(dataset.data[0], jsonDataset[0], 'returns all records');
-//   });
-
-//   t.test('With csv output', async (t) => {
-//     const CMSClient = createClient(t.datasetId, {
-//       output: 'csv',
-//     });
-
-//     const dataset = await CMSClient.select().get();
-
-//     const csvDataset = fs.readFileSync('tests/fixtures/5fr6-cch3.csv', 'utf8');
-
-//     t.same(
-//       dataset.data.split(os.EOL).slice(0, 50),
-//       csvDataset.split(os.EOL),
-//       'returns all records with csv output',
-//     );
-//   });
-// });
-
-// tap.test('Query options', async (t) => {
-//   t.test('Select a column', async (t) => {
-//     const CMSClient = createClient(t.datasetId, {
-//       output: 'json',
-//       includeMetadata: true,
-//     });
-
-//     const dataset = await CMSClient.select('nppes_provider_first_name').get();
-
-//     const jsonDataset = require('./fixtures/5fr6-cch3.json');
-
-//     const wanted = jsonDataset.map((record) =>
-//       record.nppes_provider_first_name
-//         ? { nppes_provider_first_name: record.nppes_provider_first_name }
-//         : {},
-//     );
-
-//     t.same(dataset.data.slice(0, 50), wanted, 'returns selected column');
-//   });
-
-//   t.test('Select multiple columns', async (t) => {
-//     const CMSClient = createClient(t.datasetId, {
-//       output: 'json',
-//       includeMetadata: true,
-//     });
-
-//     const wanted = jsonDataset.map((record) => {
-//       if (record['nppes_provider_first_name']) {
-//         return {
-//           npi: record.npi,
-//           nppes_provider_first_name: record.nppes_provider_first_name,
-//         };
-//       } else {
-//         return {
-//           npi: record.npi,
-//         };
-//       }
-//     });
-
-//     const dataset = await CMSClient.select(['npi', 'nppes_provider_first_name'])
-//       .limit(50)
-//       .get();
-
-//     t.same(dataset.data, wanted, 'returns selected columns');
-//   });
-
-//   t.test('With limit', async (t) => {
-//     const CMSClient = createClient(t.datasetId, {
-//       output: 'json',
-//       includeMetadata: true,
-//     });
-//     const dataset = await CMSClient.limit(50).get();
-//     t.same(dataset.data, jsonDataset, 'returns limited records');
-//   });
-
-//   t.test('Order columns', async (t) => {
-//     const CMSClient = createClient(t.datasetId, {
-//       output: 'json',
-//       includeMetadata: true,
-//     });
-
-//     const dataset = await CMSClient.order('nppes_provider_last_org_name').get();
-
-//     t.ok(dataset.data, 'returns records ordered by a column');
-//     t.end();
-//   });
-
-//   t.test('Filter by resource', async (t) => {
-//     const CMSClient = createClient(t.datasetId, {
-//       output: 'json',
-//       includeMetadata: true,
-//     });
-
-//     const wanted = jsonDataset.filter(
-//       (record) => record['nppes_provider_first_name'] == 'THOMAS',
-//     );
-
-//     const dataset = await CMSClient.filter(
-//       'nppes_provider_first_name',
-//       'THOMAS',
-//     )
-//       .limit(2)
-//       .get();
-
-//     t.same(dataset.data, wanted, 'returns filtered resource');
-//     t.end();
-//   });
-// });
-
-// tap.test('Stream option', async (t) => {
-//   const streamDestination = fs.createWriteStream(
-//     path.join(process.cwd(), 'tests', 'streamed.json'),
-//   );
-//   const CMSClient = createClient(t.datasetId, {
-//     output: 'json',
-//     includeMetadata: true,
-//     stream: streamDestination,
-//   });
-
-//   await CMSClient.select('nppes_provider_first_name').limit(50).get();
-
-//   const streamedData = require(path.join(
-//     process.cwd(),
-//     'tests',
-//     'streamed.json',
-//   ));
-
-//   t.ok(streamedData, 'streams content to writable destination');
-
-//   t.teardown(() => {
-//     fs.unlink(path.join(process.cwd(), 'tests', 'streamed.json'), (error) => {
-//       if (error) {
-//         throw new Error(error);
-//       }
-//     });
-//   });
-//   t.end();
-// });
